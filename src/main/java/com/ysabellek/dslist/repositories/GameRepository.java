420f5bb9e1d08555029e7d1de75600c6462ecc74
Ysabelle Kristinne <ysabellekristinne@outlook.com>
2025-01-20 13:40:30 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/ysabellek/dslist/repositories/GameRepository.java
@@ -0,0 +1,13 @@
+package com.ysabellek.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ysabellek.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+}
+
+
+
+

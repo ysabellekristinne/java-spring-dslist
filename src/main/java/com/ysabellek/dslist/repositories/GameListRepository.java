d5ee7869d275315de423f5fdf0755f2660153114
Ysabelle Kristinne <ysabellekristinne@outlook.com>
2025-01-22 18:29:13 -0300

    GET lists

---
+++ src/main/java/com/ysabellek/dslist/repositories/GameListRepository.java
@@ -0,0 +1,13 @@
+package com.ysabellek.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ysabellek.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
+
+
+
+
---
+++ src/main/java/com/ysabellek/dslist/services/GameListService.java
@@ -0,0 +1,24 @@
+package com.ysabellek.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ysabellek.dslist.dto.GameListDTO;
+import com.ysabellek.dslist.entities.GameList;
+import com.ysabellek.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+	
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+}
